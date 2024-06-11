cd080db73d0be5d4244a0cfde5e0100fed023fd8
Nazmul Hasan Shajib <shojib116@gmail.com>
2024-06-11 10:55:46 +0600

    3.18-completed

--- index.js
+++ index.js
@@ -18,34 +18,12 @@ app.use(
 
 app.use(express.static("dist"));
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/info", (request, response) => {
-  const personCount = persons.length;
   const now = new Date();
-  response.send(
-    `<p>Phonebook has info for ${personCount} people </p><p>${now}</p>`
+  Persons.countDocuments({}).then((result) =>
+    response.send(
+      `<p>Phonebook has info for ${result} people </p><p>${now}</p>`
+    )
   );
 });
 
@@ -53,15 +31,10 @@ app.get("/api/persons/", (request, response) => {
   Persons.find({}).then((persons) => response.json(persons));
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+app.get("/api/persons/:id", (request, response, next) => {
+  Persons.findById(request.params.id)
+    .then((result) => response.json(result))
+    .catch((error) => next(error));
 });
 
 app.post("/api/persons/", (request, response) => {
